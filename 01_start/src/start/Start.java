bf1fb93a860b9ee8634ce3103100a5d1526711db
akd4850 <akd3850@gmail.com>
2024-01-04 10:11:04 +0900

    project push

---
+++ 01_start/src/start/Start.java
@@ -0,0 +1,10 @@
+package start;
+
+public class Start 
+{
+    public static void main(String[] args)
+    {
+        //System.out.println(answer);
+        //System.out.println((num % n == 0) ? 1 : 0);
+    }
+}
